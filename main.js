d13fcacb6a6e82e1237ca7c92f21b9ba53155b86
willy836 <wilsonkimiri836@gmail.com>
2022-12-01 23:40:04 +0300

    Refactor to use JavaScript classes

--- main.js
+++ main.js
@@ -1,114 +1,135 @@
-// Add new book to collection
-const bookContainer = document.querySelector('.book-container');
-const form = document.querySelector('.form');
+/* eslint max-classes-per-file: ["error", 2] */
+
+class Book {
+  constructor(title, author, next = null) {
+    this.title = title;
+    this.author = author;
+    this.next = next;
+  }
+}
+
+class Books {
+  constructor() {
+    this.head = null;
+  }
+
+  add(title, author) {
+    const book = new Book(title, author);
+    if (this.head === null) {
+      this.head = book;
+      return;
+    }
+
+    let curr = this.head;
+
+    while (curr.next !== null) {
+      curr = curr.next;
+    }
+    curr.next = book;
+  }
+
+  remove(title, author) {
+    if (this.head === null) return;
+    let curr = this.head;
+    let prev = null;
+
+    while (curr !== null) {
+      if (curr.title === title && curr.author === author) {
+        if (prev === null) {
+          this.head = curr.next;
+        } else {
+          prev.next = curr.next;
+        }
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+}
+
+let newBooks = new Books();
+
+function displayBooks() {
+  const bklist = document.querySelector('.book-list');
+  bklist.innerHTML = '';
+
+  const booksArr = localStorage.getItem('bks') ? JSON.parse(localStorage.getItem('bks')) : [];
+  let myBook = booksArr.head;
+  while (myBook !== null) {
+    const article = document.createElement('article');
+    article.className = 'article';
+    const titleP = document.createElement('p');
+    titleP.className = 'title';
+    titleP.textContent = myBook.title;
+    article.appendChild(titleP);
+
+    const authorP = document.createElement('p');
+    authorP.className = 'author';
+    authorP.textContent = myBook.author;
+    article.appendChild(authorP);
+
+    const rmvBtn = document.createElement('button');
+    rmvBtn.className = 'remove-btn';
+    rmvBtn.textContent = 'Remove';
+    const rmvBtnDiv = document.createElement('div');
+    rmvBtnDiv.className = 'rmv-div';
+    rmvBtnDiv.appendChild(rmvBtn);
+    article.appendChild(rmvBtnDiv);
+
+    bklist.appendChild(article);
+    bklist.classList.add('show-container');
+
+    myBook = myBook.next;
+  }
+}
+
+const addBtn = document.querySelector('.add-btn');
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
+addBtn.addEventListener('click', () => {
+  if (title.value !== '' && author.value !== '') {
+    newBooks.add(title.value, author.value);
 
-const bookList = document.querySelector('.book-list');
+    localStorage.setItem('bks', JSON.stringify(newBooks));
+    title.value = '';
+    author.value = '';
+    displayBooks();
+  }
+});
 
-// LOCAL STORAGE
-function getFromLocalStorage() {
-  return localStorage.getItem('bkList') ? JSON.parse(localStorage.getItem('bkList')) : [];
-}
+function saveAddedBk() {
+  const booksArr = JSON.parse(localStorage.getItem('bks'));
 
-function addToLocalStorage(id, bk) {
-  const book = { id, bk };
-  const arr = getFromLocalStorage();
+  if (booksArr !== null) {
+    let bk = booksArr.head;
 
-  arr.push(book);
-  localStorage.setItem('bkList', JSON.stringify(arr));
-}
-
-function removeFromLocalStorage(id) {
-  let arr = getFromLocalStorage();
-  arr = arr.filter((obj) => {
-    if (obj.id !== id) {
-      return true;
+    if (bk === undefined) return;
+    newBooks = new Books();
+    while (bk !== null) {
+      newBooks.add(bk.title, bk.author);
+      bk = bk.next;
     }
-    return false;
-  });
-  localStorage.setItem('bkList', JSON.stringify(arr));
-}
-
-// remove book function
-function removeBk(e) {
-  const element = e.currentTarget.parentElement;
-  const { id } = element.dataset;
-
-  bookList.removeChild(element);
-
-  removeFromLocalStorage(id);
-}
-
-// reset inputs
-function setBackToDefault() {
-  title.value = '';
-  author.value = '';
-}
-
-//  Add book Function
-function addBook(e) {
-  e.preventDefault();
-  const titleValue = title.value;
-  const authorValue = author.value;
-  const id = new Date().getTime().toString();
-  if (titleValue && authorValue) {
-    const book = document.createElement('article');
-    book.className = 'book';
-    const attr = document.createAttribute('data-id');
-    attr.value = id;
-    book.setAttributeNode(attr);
-    book.innerHTML = `<p class="book-title">${titleValue}</p>
-        <p class="book-author">${authorValue}</p>
-        <button class="remove">Remove</button>
-        <hr>`;
-
-    const bk = {};
-    bk.title = titleValue;
-    bk.author = authorValue;
-    bk.id = id;
-
-    const removeBtn = book.querySelector('.remove');
-    removeBtn.addEventListener('click', removeBk);
-    bookList.appendChild(book);
-    bookContainer.classList.add('show-container');
-
-    addToLocalStorage(id, bk);
-
-    setBackToDefault();
   }
 }
 
-// load books function
-function loadBooks() {
-  const arr = getFromLocalStorage();
-  if (arr.length > 0) {
-    arr.forEach((buk) => {
-      const book = document.createElement('article');
-      book.className = 'book';
-      const attr = document.createAttribute('data-id');
-      attr.value = buk.id;
-      book.setAttributeNode(attr);
-      book.innerHTML = `<p class="book-title">${buk.bk.title}</p>
-            <p class="book-author">${buk.bk.author}</p>
-            <button class="remove">Remove</button>
-            <hr>`;
+window.addEventListener('load', () => {
+  displayBooks();
+  saveAddedBk();
+});
 
-      const bk = {};
-      bk.title = buk.bk.titleValue;
-      bk.author = buk.bk.authorValue;
-      bk.id = buk.id;
+const removeBtn = document.querySelector('.book-list');
 
-      const removeBtn = book.querySelector('.remove');
-      removeBtn.addEventListener('click', removeBk);
-      bookList.appendChild(book);
-      bookContainer.classList.add('show-container');
-    });
+removeBtn.addEventListener('click', (event) => {
+  const targetBk = event.target.closest('.article');
+  if (targetBk !== null && targetBk !== undefined) {
+    const nodelist = targetBk.childNodes;
+
+    const [title, author] = nodelist;
+    newBooks.remove(title.innerText, author.innerText);
+    localStorage.removeItem('bks');
+
+    localStorage.setItem('bks', JSON.stringify(newBooks));
+    saveAddedBk();
+    displayBooks();
   }
-}
-
-// Add book
-form.addEventListener('submit', addBook);
-
-// Load books on refresh
-window.addEventListener('DOMContentLoaded', loadBooks);
+});
